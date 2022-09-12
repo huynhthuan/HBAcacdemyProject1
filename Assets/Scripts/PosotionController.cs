d0bd8bbf609375e3f8c8f65ad807810e5d883d0b
huynhthuan <huynhthuanvugia@gmail.com>
2022-09-12 21:23:28 +0700

    Add try some func change position gameObject

--- Assets/Scripts/PosotionController.cs
+++ Assets/Scripts/PosotionController.cs
@@ -7,28 +7,30 @@ public class PosotionController : MonoBehaviour
     public Transform pointA;
     public Transform pointB;
 
+    public Rigidbody2D rb;
+
     public float speed;
 
     // Start is called before the first frame update
     void Start()
     {
         transform.position = pointA.position;
-
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = (pointB.position - pointA.position) * speed * Time.deltaTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Debug.Log(Vector2.Distance(transform.position, pointB.position));
-         transform.Translate((pointB.position - pointA.position) * speed * Time.deltaTime);
-
         if (Vector2.Distance(transform.position, pointB.position) < 0.1f)
         {
-            transform.position = pointB.position;
+            transform.position = pointA.position;
         }
-        // else
-        // {
-        //     transform.Translate((pointB.position - pointA.position) * speed * Time.deltaTime);
-        // }
+
+        // transform.Translate((pointB.position - pointA.position) * speed * Time.deltaTime);
+
+        // transform.position = Vector3.MoveTowards(transform.position, pointB.position, Time.deltaTime * speed);
+
+
     }
 }
